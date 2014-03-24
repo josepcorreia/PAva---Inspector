2ad3b511adaab5c3b43aba43244bd614d88712ea
Antonio Goulao <goulao80.3@gmail.com>
2014-03-24 18:44:15 +0000

    Add CommandBack, CommandForward and CommandSave to implement

---
+++ src/ist/meic/pa/CommandBack.java
@@ -0,0 +1,17 @@
+package ist.meic.pa;
+
+public class CommandBack implements Command{
+
+	@Override
+	public Object execute(Object obj, String[] line) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void execute(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
