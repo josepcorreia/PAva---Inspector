b92b8e3b77c95adc31eb811b6f84bb91ee026274
Antonio Goulao <goulao80.3@gmail.com>
2014-03-28 14:13:48 +0000

    Refactor CommandM

--- src/ist/meic/pa/CommandM.java
+++ src/ist/meic/pa/CommandM.java
@@ -8,12 +8,18 @@ import java.util.ArrayList;
 
 public class CommandM implements Command{
 
+	/* (non-Javadoc)
+	 * @see ist.meic.pa.Command#execute(java.lang.Object)
+	 */
 	@Override
 	public void execute(Object obj) {
 		// DO NOTHING
 
 	}
 
+	/* (non-Javadoc)
+	 * @see ist.meic.pa.Command#execute(java.lang.Object, java.util.ArrayList, java.util.HashMap, java.lang.String[])
+	 */
 	@Override
 	public Object execute(Object obj, ArrayList<Object> inspectedObjects,
 			HashMap<String, Object> savedObjects, String[] line)
@@ -23,31 +29,37 @@ public class CommandM implements Command{
 		while(objectClass.getSuperclass() != null) {
 			Field[] fields = objectClass.getDeclaredFields();
 
-			for(Field field : fields){
-				if(field.getName().equals(line[1])) {
-					field.setAccessible(true);
-
-					try {
-
-						Object ret = null;
-						ret = Util.convert(line[2], field.getType());
-
-						field.set(obj, ret);
-
-						new CommandI().execute(obj);
-					}  catch (IllegalArgumentException e) {
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						e.printStackTrace();
-					} catch (RuntimeException e) {
-						Util.printString("EXCEPTION "+e.getMessage());
-					}
-
-					return null;
-				}	
-			}
+			processFields(obj, line, fields);
 			objectClass = objectClass.getSuperclass();
 		}	
 		return null;
+	}
+
+	/**
+	 * @param obj
+	 * @param line
+	 * @param fields
+	 */
+	private void processFields(Object obj, String[] line, Field[] fields) {
+		for(Field field : fields){
+			if(field.getName().equals(line[1])) {
+				field.setAccessible(true);
+				try {
+					Object ret = null;
+					ret = Util.convert(line[2], field.getType());
+
+					field.set(obj, ret);
+
+					new CommandI().execute(obj);
+					return;
+				}  catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				} catch (RuntimeException e) {
+					Util.printString("EXCEPTION "+e.getMessage());
+				}
+			}	
+		}
 	}
 }
